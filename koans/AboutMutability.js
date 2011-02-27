188bfb8cb0766d289247b2486e2b065836e40935
Alberto <plagelao@gmail.com>
2011-02-27 23:03:16 +0000

    Simplifies mutability

--- koans/AboutMutability.js
+++ koans/AboutMutability.js
@@ -1,12 +1,5 @@
 describe("About Mutability", function() {
 
-  it("should expect object properties to be public and mutable", function () {
-    var aPerson = {firstname: "John", lastname: "Smith" };
-    aPerson.firstname = "Alan";
-    
-    expect(aPerson.firstname).toBe(__);
-  });
-
   it("should understand that constructed properties are public and mutable", function () {
     function Person(firstname, lastname)
     {
@@ -15,7 +8,7 @@ describe("About Mutability", function() {
     }
     var aPerson = new Person ("John", "Smith");
     aPerson.firstname = "Alan";
-    
+
     expect(aPerson.firstname).toBe(__);
   });
 
@@ -28,14 +21,13 @@ describe("About Mutability", function() {
     Person.prototype.getFullName = function () {
       return this.firstname + " " + this.lastname;
     };
-    
+
     var aPerson = new Person ("John", "Smith");
-    expect(aPerson.getFullName()).toBe(__);
-    
+
     aPerson.getFullName = function () {
       return this.lastname + ", " + this.firstname;
     };
-    
+
     expect(aPerson.getFullName()).toBe(__);
   });
 
@@ -43,7 +35,7 @@ describe("About Mutability", function() {
     function Person(firstname, lastname)
     {
       var fullName = firstname + " " + lastname;
-      
+
       this.getFirstName = function () { return firstname; };
       this.getLastName = function () { return lastname; };
       this.getFullName = function () { return fullName; };
@@ -53,15 +45,8 @@ describe("About Mutability", function() {
     aPerson.firstname = "Penny";
     aPerson.lastname = "Andrews";
     aPerson.fullName = "Penny Andrews";
-    
-    expect(aPerson.getFirstName()).toBe(__);
-    expect(aPerson.getLastName()).toBe(__);
-    expect(aPerson.getFullName()).toBe(__);
 
-    aPerson.getFullName = function () {
-      return aPerson.lastname + ", " + aPerson.firstname;
-    };
-    
+    expect(aPerson.getFirstName()).toBe(__);
     expect(aPerson.getFullName()).toBe(__);
   });
 
