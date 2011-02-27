2465ab17b42ac2f7537974283e38f3ca85ce2944
Alberto <plagelao@gmail.com>
2011-02-27 23:02:46 +0000

    removes spaces

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -1,33 +1,33 @@
 var dojox; //globals
-var df = dojox.lang.functional;    
+var df = dojox.lang.functional;
 
 describe("About Higher Order Functions", function () {
 
   it("should use filter to return array items that meet a criteria", function () {
     var numbers = [1,2,3];
     var odd = df.filter(numbers, "x % 2 !== 0");
-    
+
     expect(odd).toEqual(__);
     expect(odd.length).toBe(__);
     expect(numbers.length).toBe(__);
   });
-    
+
   it("should use 'map' to transform each element", function () {
     var numbers = [1, 2, 3];
     var numbersPlus1 = df.map(numbers, "x + 1");
-    
+
     expect(numbersPlus1).toEqual(__);
     expect(numbers).toEqual(__);
   });
-    
+
   it("should use 'reduce' to update the same result on each iteration ", function () {
     var numbers = [1, 2, 3];
     var reduction = df.reduce(numbers, "result + x");
-    
-    expect(reduction).toBe(__); 
+
+    expect(reduction).toBe(__);
     expect(numbers).toEqual(__);
   });
-    
+
   it("should use 'forEach' for simple iteration", function () {
     var numbers = [1,2,3];
     var msg = "";
@@ -36,11 +36,11 @@ describe("About Higher Order Functions", function () {
     };
 
     df.forEach(numbers, isEven);
-    
+
     expect(msg).toEqual(__);
     expect(numbers).toEqual(__);
   });
-    
+
   it("should use 'some' to apply until true", function () {
     var numbers = [1,2,3];
     var msg = "";
@@ -48,11 +48,11 @@ describe("About Higher Order Functions", function () {
       msg += item + ";";
       return (item % 2) === 0;
     };
-   
+
     expect(numbers.some(isEven)).toBeTruthy();
     expect(msg).toEqual(__);
   });
-    
+
   it("should use 'every' to applies until first false" , function () {
     var numbers = [1,2,3];
     var msg = "";
@@ -60,7 +60,7 @@ describe("About Higher Order Functions", function () {
       msg += item + ";";
       return (item % 2) === 0;
     };
-    
+
     expect(numbers.every(isEven)).toBeFalsy();
     expect(msg).toBe(__);
   });
--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -9,49 +9,48 @@ describe("About Objects", function () {
 
     it("should confirm objects are collections of properties", function () {
       expect(meglomaniac.mastermind).toBe(__);
-    }); 
+    });
 
     it("should confirm that properties are case sensitive", function () {
       expect(meglomaniac.henchwoman).toBe(__);
       expect(meglomaniac.henchWoman).toBe(__);
     });
   });
-  
+
 
   it("should know properties that are functions act like methods", function () {
-    var meglomaniac = { 
-      mastermind : "Brain", 
+    var meglomaniac = {
+      mastermind : "Brain",
       henchman: "Pinky",
       battleCry: function (noOfBrains) {
         return "They are " + this.henchman + " and the" +
           Array(noOfBrains + 1).join(" " + this.mastermind);
       }
     };
-   
-    battleCry = meglomaniac.battleCry(4);
+
+    battleCry = meglomaniac.battleCry(2);
     expect(__).toMatch(battleCry);
   });
 
   it("should confirm that when a function is attached to an object, 'this' refers to the object", function () {
-    var currentYear = 2010; // Update me!
-    var meglomaniac = { 
-      mastermind: "James Wood", 
+    var currentYear = 2011; // Update me!
+    var meglomaniac = {
+      mastermind: "James Wood",
       henchman: "Adam West",
       birthYear: 1970,
       calculateAge: function () {
-        return currentYear - this.birthYear; 
+        return currentYear - this.birthYear;
       }
     };
-   
-    expect(currentYear).toBe(__);
+
     expect(meglomaniac.calculateAge()).toBe(__);
   });
 
   describe("'in' keyword", function () {
     var meglomaniac;
     beforeEach(function () {
-      meglomaniac = { 
-        mastermind: "The Monarch", 
+      meglomaniac = {
+        mastermind: "The Monarch",
         henchwoman: "Dr Girlfriend",
         theBomb: true
       };
@@ -60,16 +59,16 @@ describe("About Objects", function () {
     it("should have the bomb", function () {
 
       hasBomb = "theBomb" in meglomaniac;
-     
+
       expect(hasBomb).toBe(__);
     });
 
     it("should not have the detonator however", function () {
 
       hasDetonator = "theDetonator" in meglomaniac;
-     
+
       expect(hasDetonator).toBe(__);
-    });    
+    });
   });
 
   it("should know that properties can be added and deleted", function () {
@@ -79,7 +78,7 @@ describe("About Objects", function () {
 
     meglomaniac.secretary = "Agent Smith";
     expect("secretary" in meglomaniac).toBe(__);
-    
+
     delete meglomaniac.henchman;
     expect("henchman" in meglomaniac).toBe(__);
   });
@@ -94,14 +93,14 @@ describe("About Objects", function () {
       var simpleCircle = new Circle(10);
       var colouredCircle = new Circle(5);
       colouredCircle.colour = "red";
-      
+
       expect(simpleCircle.colour).toBe(__);
       expect(colouredCircle.colour).toBe(__);
-    
+
       Circle.prototype.describe = function () {
         return "This circle has a radius of: " + this.radius;
       };
-    
+
       expect(simpleCircle.describe()).toBe(__);
       expect(colouredCircle.describe()).toBe(__);
   });
